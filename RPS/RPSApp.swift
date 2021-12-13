416a17e3216fd15e299b19ceb749edffa5d03064
Gennady Raster <raster.iosdev@gmail.com>
2021-12-13 09:38:58 +0300

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Gena Raster on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
